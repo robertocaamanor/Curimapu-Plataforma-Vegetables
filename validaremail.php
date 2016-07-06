43187a03cd5d7a242da62b273e937ffee900bbd2
Roberto Caamaño <raristides.caamano@gmail.com>
2016-07-06 11:59:06 -0400

    Cambio de iconos

--- validaremail.php
+++ validaremail.php
@@ -12,7 +12,7 @@ include 'src/functions/dbfunctions.php';
     <link
         href='https://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic'
         rel='stylesheet' type='text/css'>
-        <link rel="shortcut icon" href="http://i.imgur.com/akQX3eo.png">
+         <link rel="shortcut icon" href="http://i.imgur.com/XyMZ0m9.png">
 </head>
 <body>
 
